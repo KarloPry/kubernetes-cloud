8d0e638f785b3fa49dc36f10661c9fb76eb442f5
KarloPry <kpaz30@alumnos.uaq.mx>
2024-02-08 17:34:08 -0600

    Actualización de la función de búsqueda para incluir filtros de fecha y categoría.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const Morgan = require("morgan");
 
 app.use(Morgan("dev"));
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server is running on port 3000");
 });
